7c72a05dba8ccb3bc2e48a90b6607851c395ab08
Magul557 <152325482+Magul557@users.noreply.github.com>
2023-11-28 22:06:24 +0300

    Update Astrobot.py

--- Astrobot.py
+++ Astrobot.py
@@ -1,15 +1,3 @@
-import telebot
-from telebot import types
 
-bot = telebot.TeleBot('6453652066:AAHdArxm23kdPJFDgCXz4hz-q_R4ita0HaI')
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton(" Наш астрономический сайт", url = http://astro.gulden.tv/#Intro)
-    markup.add(btn1)
-    bot.send_message(message.from_user.id, "По кнопке ниже можно перейти на наш астрономический сайт", reply_markup=markup)
-
-bot.polling(none_stop = "True", interval = 0)
 
 
